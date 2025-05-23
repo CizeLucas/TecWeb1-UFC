1125233090591cec33acb37e41afc977bdbcd4d1
Samuel <samuelcamilo8@gmail.com>
2025-05-23 07:30:25 -0300

    usar sql para conectar com o banco de dados no usuariosDAO

--- src/model/usuariosDAO.java
+++ src/model/usuariosDAO.java
@@ -1,13 +1,30 @@
 package model;
 
-public class UsuariosDAO {
+import java.sql.*;
+
+public class UsuariosDAO {  
+
+    public static Connection connection;
 
     public void connect() {
-        System.out.println("Iniciando a conexão com a base de dados...");
+        try {
+            String url = "jdbc:sqlite:db/usuarios.db";
+            Connection conn = DriverManager.getConnection(url);
+            System.out.println("Conexão estabelecida!");
+            connection = conn;
+        } catch (SQLException exception) {
+            System.out.println("Erro ao conectar à base de dados: " + exception.getMessage());
+            connection = null;
+        }
     }
 
     public void close() {
-        System.out.println("Encerrando a conexão com a base de dados...");
+        try {
+            connection.close();
+            System.out.println("Encerrando a conexão com a base de dados...");
+        } catch (SQLException exception) {
+            System.out.println("Erro ao fechar a conexão: " + exception.getMessage());
+        }
     }
 
 }
