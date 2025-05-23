e268f14271a5e370347ab66e257498a71d3bb611
Samuel <samuelcamilo8@gmail.com>
2025-05-23 20:19:04 -0300

    corrigit server para usar o contexto correto

--- src/controller/Server.java
+++ src/controller/Server.java
@@ -13,7 +13,7 @@ public class Server {
     public static void main(String[] args) {
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-            server.createContext("/", new VerificaLogin());
+            server.createContext("/VerificaLogin", new VerificaLogin());
             server.setExecutor(null); // creates a default executor
             server.start();
             System.out.println("Servidor iniciado em http://localhost:8080");
