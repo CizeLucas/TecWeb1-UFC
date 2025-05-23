64e63098bfa1d138b43aed426ea4f31f0c842cff
Samuel <samuelcamilo8@gmail.com>
2025-05-22 23:08:18 -0300

    setup agora adiciona o registro de admin

--- db/esquema.sql
+++ db/esquema.sql
@@ -1,4 +1,8 @@
 CREATE TABLE usuarios (
     login VARCHAR(50) PRIMARY KEY,
-    senha VARCHAR(40) NOT NULL
+    senha_hash VARCHAR(40) NOT NULL
 );
+
+-- login: admin, senha: admin
+INSERT INTO usuarios (login, senha) VALUES
+('admin', 'd033e22ae348aeb5660fc2140aec35850c4da997');
