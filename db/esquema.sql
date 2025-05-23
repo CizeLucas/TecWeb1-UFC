ae70200fd9f5ac105eb7858609e03532df02dfe0
Samuel <samuelcamilo8@gmail.com>
2025-05-23 13:24:16 -0300

    alterar o esquema

--- db/esquema.sql
+++ db/esquema.sql
@@ -1,8 +1,11 @@
 CREATE TABLE usuarios (
     login VARCHAR(50) PRIMARY KEY,
     senha_hash VARCHAR(40) NOT NULL
+    admin BOOLEAN DEFAULT FALSE,
+    numero INT DEFAULT 0,
+    personal_text TEXT
 );
 
 -- login: admin, senha: admin
-INSERT INTO usuarios (login, senha) VALUES
-('admin', 'd033e22ae348aeb5660fc2140aec35850c4da997');
+INSERT INTO usuarios (login, senha_hash, admin) VALUES
+('admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1);
