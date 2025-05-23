f36896a1bd48b4796df7c9f580cb6f14809fe361
Samuel <samuelcamilo8@gmail.com>
2025-05-23 20:19:42 -0300

    tirar imports desnecessarios

--- src/controller/Server.java
+++ src/controller/Server.java
@@ -1,11 +1,8 @@
 package controller;
 
 import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class Server {
