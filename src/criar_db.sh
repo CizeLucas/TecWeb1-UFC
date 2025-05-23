b813c0a29fe28ef90023472177c0fc0c2cfa722b
Samuel <samuelcamilo8@gmail.com>
2025-05-22 23:14:40 -0300

    Update src com arquivos basicos no modelo MVC

--- src/criar_db.sh
+++
@@ -1,4 +0,0 @@
-echo "Criando banco local..."
-createdb meu_banco_local
-psql -d meu_banco_local -f db/schema.sql
-echo "Banco criado com sucesso!"
--- src/main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    private static final String URL = "jdbc:postgresql://localhost:5432/meu_banco_local";
-
-    public static void main(String[] args) {
-        
-    }
-}
