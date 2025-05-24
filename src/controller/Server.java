3afbfea0b550d92e9ca90c170601e4a68ae75d9f
Samuel <samuelcamilo8@gmail.com>
2025-05-24 07:53:08 -0300

    mover codigo para outro arquivo

--- src/controller/Server.java
+++ src/controller/Server.java
@@ -1,14 +1,9 @@
 package controller;
 
 import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.nio.file.Files;
 
 public class Server {
 
@@ -25,31 +20,6 @@ public class Server {
             System.out.println("Servidor iniciado em http://localhost:8080");
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    static class StaticFileHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            String path = exchange.getRequestURI().getPath();
-            File file = new File("." + path);
-            if (file.exists() && !file.isDirectory()) {
-                String mime = "text/html";
-                if (path.endsWith(".css")) mime = "text/css";
-                if (path.endsWith(".js")) mime = "application/javascript";
-                exchange.getResponseHeaders().set("Content-Type", mime);
-                byte[] bytes = Files.readAllBytes(file.toPath());
-                exchange.sendResponseHeaders(200, bytes.length);
-                OutputStream os = exchange.getResponseBody();
-                os.write(bytes);
-                os.close();
-            } else {
-                String notFound = "404 Not Found";
-                exchange.sendResponseHeaders(404, notFound.length());
-                OutputStream os = exchange.getResponseBody();
-                os.write(notFound.getBytes());
-                os.close();
-            }
         }
     }
 }
---
+++ src/controller/StaticFileHandler.java
@@ -0,0 +1,34 @@
+package controller;
+
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
+import com.sun.net.httpserver.HttpExchange;
+
+public class StaticFileHandler implements HttpHandler {
+    public void handle(HttpExchange exchange) throws IOException {
+        String path = exchange.getRequestURI().getPath();
+        File file = new File("." + path);
+        if (file.exists() && !file.isDirectory()) {
+            String mime = "text/html";
+            if (path.endsWith(".css")) mime = "text/css";
+            if (path.endsWith(".js")) mime = "application/javascript";
+            exchange.getResponseHeaders().set("Content-Type", mime);
+            byte[] bytes = Files.readAllBytes(file.toPath());
+            exchange.sendResponseHeaders(200, bytes.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(bytes);
+            os.close();
+        } else {
+            String notFound = "404 Not Found";
+            exchange.sendResponseHeaders(404, notFound.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(notFound.getBytes());
+            os.close();
+        }
+    }
+}
