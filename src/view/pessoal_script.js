16b44c8e6d046078503d65678ad06e77ede5525a
Samuel <samuelcamilo8@gmail.com>
2025-05-26 10:35:17 -0300

    adicionar função de salvar numero

--- src/view/pessoal_script.js
+++ src/view/pessoal_script.js
@@ -2,12 +2,12 @@ const usuario = sessionStorage.getItem("usuarioLogado");
 if (!usuario) {
     window.location.href = "login.html";
 } else {
-    fetch("/DadosUsuario", {
+    fetch("/RetornaDadosUsuario", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ login: usuario, pedido: "retornarDados" })
+        body: JSON.stringify({ login: usuario })
     })
     .then(response => response.json())
     .then(data => {
@@ -30,11 +30,37 @@ document.getElementById("text_box").addEventListener("submit", function(event) {
 
     console.log("texto a ser salvo: " + texto);
 
-    fetch("/DadosUsuario", {
+    fetch("/SalvaDadosUsuario", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ login: usuario, pedido: "salvarTexto", texto: texto })
+        body: JSON.stringify({ login: usuario, data_to_save: "text", text: texto })
     })
+    .catch(error => {
+        console.error("Erro ao buscar dados do usuário:", error);
+        alert("Erro ao carregar dados do usuário.");
+    });
+})
+
+document.getElementById("number_box").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    console.log("botão de salvar numero pressionado");
+
+    const numero = document.getElementById("numeroUsuario").value;
+
+    console.log("numero a ser salvo: " + numero);
+
+    fetch("/SalvaDadosUsuario", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ login: usuario, data_to_save: "number", number: numero })
+    })
+    .catch(error => {
+        console.error("Erro ao buscar dados do usuário:", error);
+        alert("Erro ao carregar dados do usuário.");
+    });
 })
