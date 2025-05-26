202cd8ea364de24fbb5bb4dd5e2547452081c281
Samuel <samuelcamilo8@gmail.com>
2025-05-26 13:17:09 -0300

    adicionar script para pagina de admin

---
+++ src/view/admin_script.js
@@ -0,0 +1,25 @@
+const usuario = sessionStorage.getItem("usuarioLogado");
+
+if (!usuario) {
+    // se não tiver usuário logado, volta para pagina de login
+    window.location.href = "login.html";
+} else {
+    fetch("/RetornaDadosUsuario", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ login: usuario })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // se o usuário logado não for admin, volta para pagina de login
+        if (!data.admin) {
+            window.location.href = "login.html";
+        }
+    })
+    .catch(error => {
+        console.error("Erro ao buscar dados do usuário:", error);
+        alert("Erro ao carregar dados do usuário.");
+    });
+}
