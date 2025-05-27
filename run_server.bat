0eac7da58f56662655569f6c1120bf715afe45fa
Samuel <samuelcamilo8@gmail.com>
2025-05-27 13:55:31 -0300

    alterado o run_bat

--- run_server.bat
+++ run_server.bat
@@ -2,10 +2,12 @@
 REM Compila e executa o Server.java
 
 REM Compilar todos os arquivos .java do src
-javac -cp "backend/lib/*" -d backend/bin backend/src/model/*.java backend/src/controller/*.java
+for /R backend/src %%f in (*.java) do (
+    javac -cp "backend/lib/*;backend/bin" -d backend/bin "%%f"
+)
 
 REM Executar o servidor
-java --enable-native-access=ALL-UNNAMED -cp "backend/bin;backend/lib/*" controller.Server
+java --enable-native-access=ALL-UNNAMED -cp "backend/bin;backend/lib/*" Server.Server
 
 pause
 REM filepath: c:\Users\user\Documents\UFC\tecweb 2025.1\TecWeb1-UFC\run_server.bat
