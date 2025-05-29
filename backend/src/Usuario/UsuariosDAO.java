41a1b798cec7f8b1f6eb10aed3f7279caea5e417
Samuel <samuelcamilo8@gmail.com>
2025-05-29 19:17:26 -0300

    port para conexão com banco de dados externo

--- backend/src/Usuario/UsuariosDAO.java
+++ backend/src/Usuario/UsuariosDAO.java
@@ -1,7 +1,10 @@
 package Usuario;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Properties;
 
 import SHA1.SHA1;
 
@@ -11,14 +14,24 @@ public class UsuariosDAO {
 
     public void connect() {
         try {
-            String url = "jdbc:sqlite:backend/db/dados.db";
-            Connection conn = DriverManager.getConnection(url);
+            Properties databaseConfig = new Properties();
+            FileInputStream fis = new FileInputStream("backend/config/dbconfig.properties");
+            databaseConfig.load(fis);
+
+            String url = databaseConfig.getProperty("db.url");
+            String user = databaseConfig.getProperty("db.user");
+            String password = databaseConfig.getProperty("db.password");
+
+            Connection conn = DriverManager.getConnection(url, user, password);
             System.out.println("Conexão com o base de dados estabelecida!");
             connection = conn;
         } catch (SQLException exception) {
             System.err.println("Erro ao conectar à base de dados: " + exception.getMessage());
             connection = null;
-        }
+        } catch (IOException exception) {
+            System.err.println("Erro ao carregar dados de acesso ao banco de dados:" + exception.getMessage());
+            connection = null;
+        } 
     }
 
     public ArrayList<Usuario> getAllUsuarios() {
