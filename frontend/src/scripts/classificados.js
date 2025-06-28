9f591db4cb7853ba91930c57d5b5b88588d1c6ae
vitoria.emillysm@gmail.com <vitoria.emillysm@gmail.com>
2025-06-28 10:46:55 -0300

    add classificados page

---
+++ frontend/src/scripts/classificados.js
@@ -0,0 +1,20 @@
+// const pageTitle = document.title;
+
+//     // Atualiza o placeholder do input com o título da página
+// const searchInput = document.getElementById("searchInput");
+// searchInput.placeholder = `Pesquisar em ${pageTitle}`;
+
+
+const toggleBtn = document.getElementById("toggleSidebar");
+const sidebar = document.getElementById("sidebar");
+
+let isSidebarOpen = true;
+
+toggleBtn.addEventListener("click", () => {
+if (isSidebarOpen) {
+    sidebar.style.display = "none";
+} else {
+    sidebar.style.display = "block";
+}
+      isSidebarOpen = !isSidebarOpen;
+});
