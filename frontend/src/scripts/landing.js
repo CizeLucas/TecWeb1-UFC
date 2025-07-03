77f93de7a85ad4f4a11e6d832c82156102438c6a
CizeLucas <cizelucas@gmail.com>
2025-07-02 21:02:26 -0300

    Add landing page navigation and button functionality

---
+++ frontend/src/scripts/landing.js
@@ -0,0 +1,22 @@
+document.getElementById("register_button").addEventListener("click", function (event) {
+    window.location.href = "registrar.html"
+});
+
+document.getElementById("login_button").addEventListener("click", function (event) {
+    window.location.href = "login.html"
+});
+
+document.getElementById("projects_page").addEventListener("click", function (event) {
+    window.alert("Página de projetos ainda não implementada.");
+    //window.location.href = "(...).html"
+});
+
+document.getElementById("classifieds_page").addEventListener("click", function (event) {
+    window.alert("Página de classificados ainda não implementada.");
+    //window.location.href = "(...).html"
+});
+
+document.getElementById("materials_page").addEventListener("click", function (event) {
+    window.alert("Página de materiais ainda não implementada.");
+    //window.location.href = "(...).html"
+});
